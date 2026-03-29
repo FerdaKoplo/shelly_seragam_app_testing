35f00140efeaa4ac20908fd530f6c942a23870a5
neverendingloneliness <sahmuraivan43@gmail.com>
2026-03-29 15:04:00 +0700

    init

---
+++ cypress.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const {
+  addCucumberPreprocessorPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor");
+const {
+  createEsbuildPlugin,
+} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://127.0.0.1:8000/",
+    setupNodeEvents: async (on, config) => {
+      await addCucumberPreprocessorPlugin(on, config);
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        }),
+      );
+      return config;
+    },
+    specPattern: "cypress/e2e/**/*.feature",
+  },
+});
